d206f022be98f1bffaf435ccf8613b70154843a4
dadasdair <zanderbaird@gmail.com>
2015-02-23 11:50:34 -0700

    The script is a thing that does an update

--- systemscriptRPM.sh
+++ systemscriptRPM.sh
@@ -4,3 +4,37 @@
 
 # not just an excuse for learning vi and git at all
 
+echo "*******************************************************************************"
+echo "Hello there, first thing's first. Who, what, when and ........................."
+echo "*******************************************************************************"
+echo "USERNAME:`whoami`"
+echo "*******************************************************************************"
+echo "`uname -nrmo` is the what"
+echo "*******************************************************************************"
+echo "When:"
+
+echo `date '+%X %p %Z %A %e %B %Y'`
+
+echo `date '+Linux is %s seconds old!'`
+echo "*******************************************************************************"
+echo "Is it time to update the system? You will need to remember your password!(Y/N)?"
+
+read answer
+	
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
+	sudo yum update && echo "That wasn't too bad was it? So long!"
+	exit 1
+else
+	echo "Perhaps next time.Fair thee well"
+	exit 1
+fi
+
+ 
+
+
+
+
+
+
+	
+
